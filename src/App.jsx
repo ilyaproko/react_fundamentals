e3c1eb5d0bb2f24a0979cab75600a5e2880009b7
ilya Clu <ilya.5597.p@gmail.com>
2023-01-31 00:34:48 +0600

    added axios, api for PostsService, useEffect()

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useEffect } from "react";
 import "./styles/App.css"
 import PostList from "./components/PostList";
 import MyInput from "./components/UI/input/MyInput";
@@ -7,36 +7,38 @@ import MySelect from "./components/UI/select/MySelect";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModel";
 import MyButton from "./components/UI/button/MyButton";
+import { usePosts } from "./hooks/usePosts";
+import axios from "axios";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
 
 function App() {
 
-  const [posts, setPosts] = useState([
-    {id: 1, title: "erj", body: "Descripti"},
-    {id: 2, title: "aa 2", body: "Descripti"},
-    {id: 3, title: "ffd 3", body: "Descripti"},
-    {id: 4, title: "kmn 4", body: "Descripti"},
-  ])
-
+  const [posts, setPosts] = useState([])
   const [filter, setFilter] = useState({ sort: "", query: "" })
   const [modal, setModal] = useState(false);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [isPostsLoading, setIsPostsLoading] = useState(false);
 
-  const sortedPosts = useMemo(() => {
-    console.log("отработала функция сортед постс");
-    if (filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
-    }
-    return posts;
-
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.includes(filter.query));
-  }, [filter.query, sortedPosts])
+  useEffect(() => {
+    fetchPosts();
+  }, [])
 
   const createPost = (newPost) => {
     setPosts([ ...posts, newPost ]);
   }
 
+  async function fetchPosts() {
+    setIsPostsLoading(true);
+    setTimeout(async () => {
+      const posts = await PostService.getAll();
+      setPosts(posts);
+      setIsPostsLoading(false);
+    }, 1000);
+    
+  }
+
+  // получаем post из дочернего компонента
   const deletePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id));
   }
@@ -59,9 +61,13 @@ function App() {
         setFilter={setFilter}
       />
 
-      <PostList remove={deletePost} posts={sortedAndSearchedPosts} title="title post 1" />
-
-      
+      {
+        isPostsLoading
+          ? <div style={{ display: "flex", justifyContent: "center", marginTop: 50}}>
+              <Loader />
+            </div>
+          : <PostList remove={deletePost} posts={sortedAndSearchedPosts} title="title post 1" />
+      }
 
     </div>
 
